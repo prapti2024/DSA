b8b4032551e4b2b674e3891ba91136316ffb5429
prapti2024 <155229094+prapti2024@users.noreply.github.com>
2025-12-20 15:57:46 +0545

    Time: 0 ms (100.00%) | Memory: 13 MB (90.18%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        hashmap = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in hashmap:
+                return (hashmap[diff],i)
+            hashmap[num] = i
+        
